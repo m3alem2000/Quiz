22f7f4834093a0f9beb9267109e25ef4cb009c1f
Mohammad Ramadan <m3alem@hotmail.com>
2017-06-27 23:38:04 -0400

    third commit

--- app/src/main/java/com/test/fifthgrademath/QuizDeskBoardActivity.java
+++ app/src/main/java/com/test/fifthgrademath/QuizDeskBoardActivity.java
@@ -11,7 +11,6 @@ public class QuizDeskBoardActivity extends AppCompatActivity
 {
 
     Button btnStart;
-    Button btnInstruction;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
